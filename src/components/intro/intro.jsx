1038cb46d1b879acb014d519bd30a7d2aa6d5cea
Ebru Rice <ebruerenb@gmail.com>
2022-08-02 22:33:26 -0700

    Add css design for intro component

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import "./intro.css"
 
 const Intro = () => {
   return (
-    <div>
-      This is the introduction page
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is  </h2>
+          <h1 className="i-name">Andrew Rice </h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Actor</div>
+              <div className="i-title-item">Producer</div>
+              <div className="i-title-item">Writer</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <div className="i-right">right</div>
     </div>
   )
   
