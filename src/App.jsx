b016d9b8235799b0abe112c6b8ad9ae365dcc251
Ebru Rice <ebruerenb@gmail.com>
2022-08-06 19:23:00 -0700

    Add dark mode toggle component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import Intro from "./components/intro/Intro";
 import About from "./components/about/About"
 import WorkList from "./components/workList/WorkList";
 import Contact from "./components/contact/Contact";
+import Toggle from "./components/toggle/Toggle";
+
 
 
 const App = () => {
   return (
     <div>
+      <Toggle/>
       <Intro/>
       <About/>
       <WorkList/>
